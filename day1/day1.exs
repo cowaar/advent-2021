ed2706daa4f5cbf85475b8b29badaecf71e458bc
Alex <alexandercowan@quantexa.com>
2021-12-01 17:59:13 +0000

    day 1 part 2 done (hacked, could clean)

--- day1/day1.exs
+++ day1/day1.exs
@@ -1,9 +1,42 @@
 defmodule Day1 do
   def run() do
     input = parse_input()
-    IO.puts(loop(input, 0))
+    IO.puts(Part1.loop(input, 0))
+    IO.puts(Part2.loop(input, 0))
   end
 
+  defp parse_input() do
+    File.read!("input.txt")
+    |> String.split("\r\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
+defmodule Part2 do
+  def acc_if_set_higher(list, acc) do
+    set = Enum.take(list, 4)
+
+    cond do
+      length(set) < 4 ->
+        acc
+
+      Enum.take(set, 3) |> Enum.sum() < Enum.take(set, -3) |> Enum.sum() ->
+        acc + 1
+
+      true ->
+        acc
+    end
+  end
+
+  def loop([], acc), do: acc
+
+  def loop(list, acc) do
+    new_acc = acc_if_set_higher(list, acc)
+    loop(tl(list), new_acc)
+  end
+end
+
+defmodule Part1 do
   def acc_if_higher(list, acc) do
     [head | tail] = list
 
@@ -24,12 +57,6 @@ defmodule Day1 do
   def loop(list, acc) do
     new_acc = acc_if_higher(list, acc)
     loop(tl(list), new_acc)
-  end
-
-  defp parse_input() do
-    File.read!("input.txt")
-    |> String.split("\r\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
   end
 end
 
