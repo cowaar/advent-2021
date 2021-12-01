8ffb47efefdfc3692420eea5d8e993d5bd736ba7
Alex <alexandercowan@quantexa.com>
2021-12-01 13:59:02 +0000

    added day1 part 1

---
+++ day1/day1.exs
@@ -0,0 +1,38 @@
+defmodule Day1 do
+  def run() do
+    input = parse_input()
+    IO.puts(loop(input, 0))
+  end
+
+  def acc_if_higher(list, acc) do
+    [head | tail] = list
+
+    cond do
+      tail == [] ->
+        acc
+
+      head < hd(tail) ->
+        acc + 1
+
+      true ->
+        acc
+    end
+  end
+
+  def loop([], acc), do: acc
+
+  def loop(list, acc) do
+    new_acc = acc_if_higher(list, acc)
+    IO.inspect(list)
+    IO.inspect(acc)
+    loop(tl(list), new_acc)
+  end
+
+  defp parse_input() do
+    File.read!("input.txt")
+    |> String.split("\r\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+end
+
+Day1.run()
