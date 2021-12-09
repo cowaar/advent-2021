befea08fff41716845b5b7caecad1f2d4dc0c826
Alex <alexandercowan@quantexa.com>
2021-12-09 21:42:36 +0000

    day3 finished

--- day3/day3.exs
+++ day3/day3.exs
@@ -1,30 +1,75 @@
 defmodule Day3 do
+  def parse_binary(ls) do
+    Enum.join(ls)
+    |> String.to_integer(2)
+  end
+
   def run() do
-    input = parse_input()
-    parse_gamma = fn ls -> parse_rate(ls, &Enum.max_by/2) end
-    parse_epsilon = fn ls -> parse_rate(ls, &Enum.min_by/2) end
+    input = Part1.parse_input()
+    parse_max = fn ls -> parse_rate(ls, &Enum.max_by/2, "1") end
+    parse_min = fn ls -> parse_rate(ls, &Enum.min_by/2, "0") end
 
-    gamma =
-      Enum.map(input, parse_gamma)
-      |> Enum.join()
-      |> String.to_integer(2)
+    # gamma =
+    #   Enum.map(input, parse_max)
+    #   |> Enum.join()
+    #   |> String.to_integer(2)
 
-    epsilon =
-      Enum.map(input, parse_epsilon)
-      |> Enum.join()
-      |> String.to_integer(2)
+    # epsilon =
+    #   Enum.map(input, parse_min)
+    #   |> Enum.join()
+    #   |> String.to_integer(2)
 
-    IO.puts("gamma: #{gamma}  epsilon: #{epsilon}")
-    IO.puts("gamma * epsilon = #{gamma * epsilon}")
+    # IO.puts("gamma: #{gamma}  epsilon: #{epsilon}")
+    # IO.puts("gamma * epsilon = #{gamma * epsilon}")
+
+    oxygen =
+      Part2.parse_rows()
+      |> Part2.loop(0, parse_max)
+      |> parse_binary()
+
+    co2 =
+      Part2.parse_rows()
+      |> Part2.loop(0, parse_min)
+      |> parse_binary()
+
+    IO.puts("oxygen: #{oxygen}  co2: #{co2}")
+    IO.puts("oxygen * co2 = #{oxygen * co2}")
   end
 
-  def parse_rate(ls, fun) do
-    Enum.frequencies(ls)
-    |> fun.(&elem(&1, 1))
-    |> elem(0)
+  def parse_rate(ls, fun, default) do
+    frequencies = Enum.frequencies(ls)
+
+    if frequencies["0"] == frequencies["1"] do
+      default
+    else
+      fun.(frequencies, &elem(&1, 1))
+      |> elem(0)
+    end
+  end
+end
+
+defmodule Part2 do
+  def loop(lists, _i, _fun) when length(lists) == 1 do
+    IO.puts("one left! its this: ")
+    IO.inspect(lists)
   end
 
-  defp parse_input() do
+  def loop(lists, i, fun) do
+    ith_digits = Enum.map(lists, fn x -> Enum.at(x, i) end)
+    desired_digit = fun.(ith_digits)
+    filtered_lists = Enum.filter(lists, fn list -> Enum.at(list, i) == desired_digit end)
+    loop(filtered_lists, i + 1, fun)
+  end
+
+  def parse_rows() do
+    File.read!("input.txt")
+    |> String.split("\r\n", trim: true)
+    |> Enum.map(fn x -> String.split(x, "", trim: true) end)
+  end
+end
+
+defmodule Part1 do
+  def parse_input() do
     File.read!("input.txt")
     |> String.split("\r\n", trim: true)
     |> Enum.map(fn x -> String.split(x, "", trim: true) end)
